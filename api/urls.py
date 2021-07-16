d453e5a77f06b8e14fddf480d65800e1e52b7c5c
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-17 03:34:24 +0800

    added CRUD functionality in the API

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('todos', views.TodoListCreate.as_view()),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroyAPIView.as_view()),
     path('todos/completed', views.TodoCompletedList.as_view()),
 ]
