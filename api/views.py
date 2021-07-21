602fcc05dd62230d76d66bd096a4f878fb1eb884
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-22 07:39:57 +0800

    added the commands for getting the token of an existing user, id of the completed todo and the remaining to do of an existing user using a token

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from django.db import IntegrityError
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.contrib.auth import authenticate
 
 
 @csrf_exempt
@@ -22,6 +23,21 @@ def signup(request):
             return JsonResponse({'token': str(token)}, status=201)
         except IntegrityError:
             return JsonResponse({'error': 'That username has already been taken. Please choose a new username'}, status=400)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is None:
+            return JsonResponse({'error': 'Could not login. Please check username and password'}, status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=200)
+
 
 class TodoCompletedList(generics.ListAPIView):
     serializer_class = TodoSerializer
--- commands.sh
+++ commands.sh
@@ -12,3 +12,12 @@ $ curl -X "POST" http://127.0.0.1:8000/api/signup -H "Content-Type: application/
 
 # getting todos using token
 curl -X GET http://127.0.0.1:8000/api/todos -H "Authorization: Token $Token_HERE}"
+
+# getting token for an existing user
+curl -X "POST" http://127.0.0.1:8000/api/login -H 'Content-Type: application/json' -d '{"username": "blah5", "password": "blashword"}'
+
+# getting the id of the completed todo by a user using a token
+curl -X "PUT" http://127.0.0.1:8000/api/todos/9/complete -H 'Content-Type: application/json' -H "Authorization: Token 86705803e1b305c2a9172f8b0de4baf82c8c6f7f"
+
+# getting the remaining todo of a user
+curl http://127.0.0.1:8000/api/todos -H 'Content-Type: application/json' -H "Authorization: Token 18abb7cf235bf7e90055c7e26c05327088435c83"
