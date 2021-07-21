215560ba9ef05a8313d06de8834cef486affbe20
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-22 06:25:08 +0800

    added commands to getting todos via token

--- commands.sh
+++ commands.sh
@@ -1,9 +1,14 @@
 
 curl http://127.0.0.1:8000/api/todos -u {username}:{password}
 
-# signing up viw POST using a token using CMD
+# signing up via POST using a token using CMD
 curl -X "POST" http://127.0.0.1:8000/api/signup -H "Content-Type: application/json" -d @json.txt
 
-# signing up viw POST using a token using terminal
+# signing up via POST using a token using terminal
 $ curl -X "POST" http://localhost:8000/api/signup -H 'Content-Type: application/json' -d '{"username": "blah", "password": "password"}'
 
+# signing up via POST using terminal
+$ curl -X "POST" http://127.0.0.1:8000/api/signup -H "Content-Type: application/json" -d '{"username": "badum", "password": "password"}'
+
+# getting todos using token
+>curl -X GET http://127.0.0.1:8000/api/todos -H "Authorization: Token $Token_HERE}"
